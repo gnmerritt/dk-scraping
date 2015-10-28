3fc1d1ecaa3dbd935bb94e3a1a2af0530ccad0ea
Nathan Merritt <gnm@the-merritts.net>
2015-10-27 23:43:00 -0400

    added slack logging to flask app

--- racetrack/app/__init__.py
+++ racetrack/app/__init__.py
@@ -1,10 +1,23 @@
+import logging
+import slacker_log_handler as slh
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config.from_object('racetrack.app.default_settings')
 app.config.from_envvar('RACETRACK_SETTINGS', silent=True)
 
 db = SQLAlchemy(app)
+
+if not app.debug:
+    handler = slh.SlackerLogHandler(
+        app.config['SLACK_API_KEY'],
+        app.config['SLACK_CHANNEL'],
+        username=app.config['SLACK_USERNAME']
+    )
+    handler.setLevel(logging.ERROR)
+    app.logger.addHandler(handler)
 
 from . import models
