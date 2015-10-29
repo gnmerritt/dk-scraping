97faf5da993d8fd121268b60b52fb62cfd6740d0
Nathan Merritt <gnm@the-merritts.net>
2015-10-28 22:16:51 -0400

    unit tests pass again - fixed up models

--- racetrack/app/models.py
+++ racetrack/app/models.py
@@ -5,7 +5,7 @@ from . import db
 
 class Player(db.Model):
     __tablename__ = "players"
-    id = db.Column(db.Integer, primary_key=True) # eg 9738
+    id = db.Column(db.Integer, primary_key=True)  # eg 9738
     created = db.Column(db.DateTime)
 
     first = db.Column(db.String(40))
@@ -31,52 +31,68 @@ class ExternalPlayer(db.Model):
             .format(id=self.player_id, eid=self.external_id, s=self.site)
 
 
-# Dictionary for lineups. 
-    # eg  14 / 201510261010 / 'Chargers' / 'NFL' 
+# Dictionary for lineups.
+    # eg  14 / 201510261010 / 'Chargers' / 'NFL'
 class Lineup(db.Model):
     __tablename__ = "lineups"
-    
-    id = db.Column(db.Integer, primary_key=True) # eg 14
+
+    id = db.Column(db.Integer, primary_key=True)  # eg 14
     created = db.Column(db.DateTime)
 
-    entryname = db.Column(db.String(100)) # eg jockeyjz; associated with either of our DK/FD/.. profiles
+    # eg jockeyjz; associated with either of our DK/FD/.. profiles
+    entryname = db.Column(db.String(100))
 
-    team = db.Column(db.String(100)) # eg 'Steelers'
-    sport = db.Column(db.String(10)) # eg 'NFL'
+    team = db.Column(db.String(100))  # eg 'Steelers'
+    sport = db.Column(db.String(10))  # eg 'NFL'
 
     def __repr__(self):
         return "Lineup(id={id}, team={t}, sport={s})" \
             .format(id=self.id, t=self.team, s=self.sport)
 
-    
-# Dictionary for EXTERNAL lineups # eg => [13,183801480,maxdalury (39/47),0,239.42,QB Andrew Luck RB Lamar Miller RB Todd Gurley WR Larry Fitzgerald WR Julio Jones WR T.Y. Hilton TE Crockett Gillmore]
+
 class ExternalLineup(db.Model):  # these are all historical
+    """Dictionary for EXTERNAL lineups
+    eg => [13,183801480,maxdalury (39/47),0,239.42,QB Andrew Luck
+           RB Lamar Miller RB Todd Gurley WR Larry Fitzgerald
+           WR Julio Jones WR T.Y. Hilton TE Crockett Gillmore]
+    """
     __tablename__ = "external_lineups"
 
-    lineup_id = db.Column(db.Integer, db.ForeignKey("lineups.id"), index=True) # Import a lot of other people's lineups? OK with me. 
-    external_id = db.Column(db.Integer) # ['EntryId'] column from historical contest standings
-    site = db.Column(db.String(5)) 
-    entryname = db.Column(db.String(140)) # eg 'maxdalury'
-    
+    # Import a lot of other people's lineups? OK with me.
+    lineup_id = db.Column(db.Integer, db.ForeignKey("lineups.id"), index=True)
+    # ['EntryId'] column from historical contest standings
+    external_id = db.Column(db.Integer)
+    site = db.Column(db.String(5))
+    entryname = db.Column(db.String(140))  # eg 'maxdalury'
+
     team = db.Column(db.String(100))
     sport = db.Column(db.String(5))
 
-    rel_rank = db.Column(db.Float) # eg .0294859023 == top 3%
-    contest = db.Column(db.Integer) # contest id handed down from DK Api
+    rel_rank = db.Column(db.Float)  # eg .0294859023 == top 3%
+    contest = db.Column(db.Integer)  # contest id handed down from DK Api
+
+    primary_key = db.PrimaryKeyConstraint(lineup_id, site)
 
 
-# Join players and lineups. 
-    # eg  9738 / 14 / 'WR'
-class PlayerLineup(db.Model): # actually a join... (vs Model?)
+class PlayerLineup(db.Model):  # actually a join... (vs Model?)
+    """
+    Join players and lineups.
+    eg  9738 / 14 / 'WR'
+    """
     __tablename__ = "playerlineups"
 
     player_id = db.Column(db.Integer, db.ForeignKey("players.id"), index=True)
-    lineup_id = db.Column(db.Integer, db.ForeignKey("lineups.id"), index=True) # for internal or external lineups
+    # for internal or external lineups
+    lineup_id = db.Column(db.Integer, db.ForeignKey("lineups.id"), index=True)
 
-    player_position = db.Column(db.String(40)) # in case player pos changes (Randy Moss plays WR and then TE or whatever). not indexed on.
-        # nb QB Tom Brady RB Lamar Miller RB Todd Gurley WR Nate Washington WR Julio Jones WR Mike Evans TE Ladarius Green FLEX Rob 
+    player_position = db.Column(db.String(40))
+    # in case player pos changes (Randy Moss plays WR and then TE or whatever).
+    # nb QB Tom Brady RB Lamar Miller RB Todd Gurley WR Nate Washington
+    #    WR Julio Jones WR Mike Evans TE Ladarius Green FLEX Rob
+
+    primary_key = db.PrimaryKeyConstraint(player_id, lineup_id)
 
     def __repr__(self):
         return "PlayerLineup(player_id={p}, line_id={l}, player_position={pos})" \
-            .format(id=self.player_id, l=self.line_id, pos=self.player_position)
-
+            .format(id=self.player_id, l=self.line_id,
+                    pos=self.player_position)
